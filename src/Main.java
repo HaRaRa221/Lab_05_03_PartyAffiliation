609376f2cdafe14d094e1f77c875839965dfdff4
Raymond Ha <Haro@mail.uc.edu>
2022-09-22 17:25:30 -0400

    Party Affiliation added with Switch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static <string> void main(String[] args)
+    {
+        String personalResponse = "";
+        String Democrat;
+        String Republican;
+        String Independent;
+        String Other;
+
+        String menu = "D - Democrat R - Republican I - Independent O - Other";
+        System.out.println(menu);
+        personalResponse = "O";
+        personalResponse = personalResponse.toUpperCase();
+
+        System.out.println("Simulating party affiliation: " + personalResponse);
+
+        switch (personalResponse)
+        {
+            case "D":
+            System.out.println("You get a Democratic Donkey!");
+            break;
+            case "R":
+            System.out.println("You get a Republican Elephant!");
+            break;
+            case "I":
+            System.out.println("You are an Independent Man!");
+            break;
+            case "O":
+            System.out.println("You really have no party. No fun!");
+            break;
+
+        }
     }
 }
